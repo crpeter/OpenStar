ff5fb162ea77da437d501ade8ff74bb63d8d1a43
petercody <peter.cody14@gmail.com>
2026-08-09 13:28:27 -0500

    Initial Commit

---
+++ OpenStar/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  OpenStar
+//
+//  Created by Cody Peter on 8/9/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ OpenStarTests/OpenStarTests.swift
@@ -0,0 +1,18 @@
+//
+//  OpenStarTests.swift
+//  OpenStarTests
+//
+//  Created by Cody Peter on 8/9/26.
+//
+
+import Testing
+
+struct OpenStarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
